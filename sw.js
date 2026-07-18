1b20f057f2aa338d41e8236ea69ccd4ba0403803
canderson7465-cpu <canderson7465@gmail.com>
2026-07-18 09:12:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'pocket-ledger-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
